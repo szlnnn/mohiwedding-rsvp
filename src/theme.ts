e407d73e67afb54d0b93c79cdd9f6047f8814f7f
csortansz <csortansz@innomatrix.hu>
2025-04-27 13:40:38 +0200

    developing rsvp functions

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,15 @@
-import { extendTheme, theme as baseTheme } from '@chakra-ui/react'
+import { extendTheme, theme as baseTheme } from '@chakra-ui/react';
 
 const config = {
   initialColorMode: 'light',
   useSystemColorMode: false,
-}
+};
 
 const theme = extendTheme({
   config,
   fonts: {
-    heading: `'Playfair Display', ${baseTheme.fonts?.heading}`,
-    body: `'Open Sans', ${baseTheme.fonts?.body}`,
+    heading: `'Garamond', ${baseTheme.fonts?.heading}`,
+    body: `'Garamond', ${baseTheme.fonts?.body}`,
   },
   colors: {
     brand: {
@@ -27,24 +27,24 @@ const theme = extendTheme({
       },
     },
   },
- components: {
-      Switch: {
-        baseStyle: {
-          track: {
-            bg: "#e0d6c9", // Soft sand (visible on beige)
-            _checked: {
-              bg: "#A3C49A", // your theme's green
-            },
+  components: {
+    Switch: {
+      baseStyle: {
+        track: {
+          bg: '#e0d6c9', // Soft sand (visible on beige)
+          _checked: {
+            bg: '#A3C49A', // your theme's green
           },
-          thumb: {
-            bg: "#b29785", // muted mocha for visibility
-            _checked: {
-              bg: "white",
-            },
+        },
+        thumb: {
+          bg: '#b29785', // muted mocha for visibility
+          _checked: {
+            bg: 'white',
           },
         },
       },
-    }
-})
+    },
+  },
+});
 
-export default theme
+export default theme;
